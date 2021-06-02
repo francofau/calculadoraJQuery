2b8062210db6c713b0e7319bacb3abffea347692
jceperuelo <jordiceperuelo@gmail.com>
2021-06-02 19:46:20 +0200

    Realizado en clase

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
 /* Javascript */
+
+// Para poner en lista el resultado obtenido (cuando apretemos el igual)
+
+// var identf = ""
+// function igual(id){
+//     document.getElementById("demo").innerHTML += "Clicked!<br>";}
+
+
+
+$(document).ready(function(){
+    $("#igual").on('click', function(e){
+        e.preventDefault();
+       $("#demo").html("click"); 
+    })
+
+
+
+
+
+
+
+    
+})
