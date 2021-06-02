aad4018fb0b6365275562e0b3d035b3ca8a484d8
Franco Fauda <63079368+francofau@users.noreply.github.com>
2021-06-02 23:50:36 +0200

    avances
    Modifiqué parte de la estructura HTML para acercarlo mas al modelo final y agrego funcionalidad a los botones desde el 0 al 9 además de poder usar realizar las operaciones básicas. Por otra parte se suma el favicon del proyecto

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,50 @@
-/* Javascript */
+$(document).ready(function(){
+		$("#textoPantalla").val("");
+		op="";
+		$("#textoPantalla").on("click",
+		function(){ $("#textoPantalla").val("");});
+		$("#btnC").on("click",
+		function(){ $("#textoPantalla").val("");
+		op="";
+		});
+
+/* muestra en "pantalla" lo que se presiona */
+$(".val").on("click",
+		function(e) {
+			e.preventDefault();
+			var a = $(this).attr("href");
+			$("#textoPantalla").val($("#textoPantalla").val()+a);
+		});			
+
+$("#btnSuma").on("click",
+		function() { num = $("#textoPantalla");
+					 acc = num.val();
+					  op= op+acc+"+";
+					  $("#textoPantalla").val("");});
+$("#btnResta").on("click",
+		function() { num = $("#textoPantalla");
+					 acc = num.val();
+					  op= op+acc+"-";
+					  $("#textoPantalla").val("");});
+$("#btnMulti").on("click",
+		function() { num = $("#textoPantalla");
+					 acc = num.val();
+					  op= op+acc+"*";
+					  $("#textoPantalla").val("");});
+$("#btnDivi").on("click",
+		function() { num = $("#textoPantalla");
+					 acc = num.val();
+					  op= op + acc + "/" ;
+					  $("#textoPantalla").val("");});
+
+ 
+$("#btnIgual").on("click",
+		function(){
+			var num = $("#textoPantalla");
+			acc = num.val();
+			op+= acc;
+			$("#textoPantalla").val (eval(op));
+			op="";
+
+        })
+});
