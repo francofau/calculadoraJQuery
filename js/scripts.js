e22b474982db0ef6f9742e2b1b9345c55baaf54e
Franco Fauda <63079368+francofau@users.noreply.github.com>
2021-06-03 11:33:27 +0200

    css+funcionalidades
    Agregué funcionamiento a algunos botones más. También sumé iconos de FontAwesome y algunos estilos extras en CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,95 @@
 $(document).ready(function(){
-		$("#textoPantalla").val("");
-		op="";
-		$("#textoPantalla").on("click",
-		function(){ $("#textoPantalla").val("");});
-		$("#btnC").on("click",
-		function(){ $("#textoPantalla").val("");
-		op="";
-		});
+	/* Agregar al listado de calculo histórico */
+	 $("#btnIgual").on('click', function(e){
+        e.preventDefault();
+       $("#listadoHistorico").html("agregar al historial"); 
+    })
 
-/* muestra en "pantalla" lo que se presiona */
-$(".val").on("click",
-		function(e) {
-			e.preventDefault();
-			var a = $(this).attr("href");
-			$("#textoPantalla").val($("#textoPantalla").val()+a);
-		});			
+	$("#textoPantalla").val("");
+	op="";
+	$("#textoPantalla").on("click",
+	function(){ $("#textoPantalla").val("");});
+	$("#btnC").on("click",
+	function(){ $("#textoPantalla").val("");
+	op="";
+	});
+	/* PI */
+	$("#btnPI").on("click",
+	function(){ $("#textoPantalla").val("3.141592653589");
+	op="";
+	});
+	/* número E */
+	$("#btnE").on("click",
+	function(){ $("#textoPantalla").val("2.718281828459");
+	op="";
+	});
+	/* Paréntesis REVISAR*/
+	$("#btnParI").on("click",
+	function(){ $("#textoPantalla").val("(");
+	op="";
+	});
+	$("#btnParD").on("click",
+	function(){ $("#textoPantalla").val(")");
+	op="";
+	});
 
-$("#btnSuma").on("click",
-		function() { num = $("#textoPantalla");
-					 acc = num.val();
-					  op= op+acc+"+";
-					  $("#textoPantalla").val("");});
-$("#btnResta").on("click",
-		function() { num = $("#textoPantalla");
-					 acc = num.val();
-					  op= op+acc+"-";
-					  $("#textoPantalla").val("");});
-$("#btnMulti").on("click",
-		function() { num = $("#textoPantalla");
-					 acc = num.val();
-					  op= op+acc+"*";
-					  $("#textoPantalla").val("");});
-$("#btnDivi").on("click",
-		function() { num = $("#textoPantalla");
-					 acc = num.val();
-					  op= op + acc + "/" ;
-					  $("#textoPantalla").val("");});
+	/* muestra en "pantalla" lo que se presiona */
+	$(".val").on("click",
+			function(e) {
+				e.preventDefault();
+				var a = $(this).attr("href");
+				$("#textoPantalla").val($("#textoPantalla").val()+a);
+			});
+	/* Suma */
+	$("#btnSuma").on("click",
+			function() { num = $("#textoPantalla");
+						acc = num.val();
+						op= op+acc+"+";
+						$("#textoPantalla").val("");});
+	/* Resta */
+	$("#btnResta").on("click",
+			function() { num = $("#textoPantalla");
+						acc = num.val();
+						op= op+acc+"-";
+						$("#textoPantalla").val("");});
+	/* Multiplicación */
+	$("#btnMulti").on("click",
+			function() { num = $("#textoPantalla");
+						acc = num.val();
+						op= op+acc+"*";
+						$("#textoPantalla").val("");});
+	/* División */
+	$("#btnDivi").on("click",
+			function() { num = $("#textoPantalla");
+						acc = num.val();
+						op= op + acc + "/" ;
+						$("#textoPantalla").val("");});
 
- 
-$("#btnIgual").on("click",
-		function(){
-			var num = $("#textoPantalla");
-			acc = num.val();
-			op+= acc;
-			$("#textoPantalla").val (eval(op));
-			op="";
+	/* Igual */
+	$("#btnIgual").on("click",
+			function(){
+				var num = $("#textoPantalla");
+				acc = num.val();
+				op+= acc;
+				$("#textoPantalla").val (eval(op));
+				op="";
 
-        })
+			})
+
+	// otras operaciones
+		$("#btnX2").on("click",
+			function() {
+				var num = $("#textoPantalla");
+				$("#textoPantalla").val(num.val() * num.val());
+			});	
+		$("#btn1x").on("click",
+			function() {
+				var num = $("#textoPantalla");
+				$("#textoPantalla").val(1 / num.val());
+			});	
+		$("#btnRaiz").on("click",
+			function() {
+				var num = $("#textoPantalla");
+				$("#textoPantalla").val( Math.sqrt(num.val()));
+			});	
 });
